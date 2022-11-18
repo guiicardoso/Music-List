2d28080fb1e48492731746d4114a0780aff118fa
guiicardoso <guilherme_c_@hotmail.com>
2022-11-18 21:26:45 +0000

    add metodos

--- app.py
+++ app.py
@@ -1,14 +1,24 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+tasks = [
+    {'name': 'Estudar' , 'finished' : False},
+    {'name': 'Dormir', 'finished': True},
+    {'name': 'Comer', 'finished': True}
+]
+
+
 @app.route('/')
 def home():
-    return render_template('home.html')
+    return render_template('home.html', tasks = tasks)
 
-@app.route('/bey')
-def bye():
-    return ' Bye'
+@app.route('/create', methods={'POST'})
+def create():
+    name = request.form['name']
+    task = {'name': name, 'finished': False}
+    tasks.append(task)
+    return render_template('home.html', tasks = tasks)
 
 app.run(debug=True)
 
