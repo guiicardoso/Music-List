98833531315e603e1f7c61f19caa556c5435d9e6
guiicardoso <101534496+guiicardoso@users.noreply.github.com>
2022-11-30 22:34:03 -0300

    Add files via upload
    Correção do Delete 
    estilização do pág. html  ( input, tamanho botão, visualização tabela, msg de lista vazia)
    implementação da rota

--- app.py
+++ app.py
@@ -1,24 +1,29 @@
-from flask import Flask, render_template, request
+from flask import Flask,render_template, request, url_for, redirect
 
 app = Flask(__name__)
 
-tasks = [
-    {'name': 'Estudar' , 'finished' : False},
-    {'name': 'Dormir', 'finished': True},
-    {'name': 'Comer', 'finished': True}
-]
+musicas =[]
 
+# musicas =[
+#     {'name': 'Estudar', 'finished': False},
+#     {'name': 'Dormir', 'finished': True},
+#     {'name': 'Comer', 'finished': True},
+# ]
 
 @app.route('/')
 def home():
-    return render_template('home.html', tasks = tasks)
+    return render_template('home.html', musicas=musicas)
 
 @app.route('/create', methods=['POST'])
 def create():
     name = request.form['name']
-    task = {'name': name, 'finished': False}
-    tasks.append(task)
-    return render_template('home.html', tasks = tasks)
+    musica = {'name' : name, 'finished': False}
+    musicas.append(musica)
+    return redirect(url_for('home'))    
 
+@app.route('/Deletar/<name>')
+def deletar(name):
+    musica = {'name' : name, 'finished': False}
+    musicas.remove(musica)        
+    return redirect(url_for('home'))
 app.run(debug=True)
-
