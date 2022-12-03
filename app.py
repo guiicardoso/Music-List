a7f02295f5c42e126961170bd375c4d1af7820a3
guiicardoso <101534496+guiicardoso@users.noreply.github.com>
2022-12-03 18:29:25 -0300

    Add files via upload
    Alteração no  Json

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask,render_template, request, url_for, redirect
-
+import json
 app = Flask(__name__)
 
 musicas =[]
@@ -12,18 +12,28 @@ musicas =[]
 
 @app.route('/')
 def home():
+    with open ("app.json", 'r') as f:
+        musicas = json.load(f)
     return render_template('home.html', musicas=musicas)
 
 @app.route('/create', methods=['POST'])
 def create():
     name = request.form['name']
     musica = {'name' : name, 'finished': False}
+    with open ("app.json", 'r') as f:
+        musicas = json.load(f)
     musicas.append(musica)
+    with open("app.json", 'w') as f:
+        json.dump(musicas,f, indent = 2)
     return redirect(url_for('home'))    
 
 @app.route('/Deletar/<name>')
 def deletar(name):
+    with open ("app.json", 'r') as f:
+        musicas = json.load(f)
     musica = {'name' : name, 'finished': False}
-    musicas.remove(musica)        
+    musicas.remove(musica)
+    with open("app.json", 'w') as f:
+        json.dump(musicas,f, indent = 2)        
     return redirect(url_for('home'))
 app.run(debug=True)
